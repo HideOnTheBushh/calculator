9dffe6b603b2801d662d4ccf9bbfe78f36dd0c8e
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-24 11:39:57 +0100

    Operate function and basic variables

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // First number, second number and operator between them
+    let a;
+    let b;
+    let operator;
+})
+
+
+
+
+
+
+// Takes 3 arguments, calls function based on operator
+function operate (a, operator, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } else {
+        return "Error";
+    }
+}
+
 function add (a, b) {
     return a + b;
 }
