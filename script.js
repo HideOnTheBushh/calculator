22ad34ae9248e0dc64436e4ad07ca059f1845924
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-28 14:30:09 +0100

    Move code into functions

--- script.js
+++ script.js
@@ -7,87 +7,138 @@ document.addEventListener("DOMContentLoaded", () => {
     let isEqual = false;
     const display = document.querySelector(".display");
 
+    
     // Clears the display
     document.querySelector(".clear").addEventListener("click", () => {
-        display.textContent = "0";
-        a = 0;
-        b = 0;
-        let isOperator = false;
-        let isEqual = false;
-        let operator = "";
+        clear();
     })
 
     document.querySelectorAll(".number").forEach(number => {
         number.addEventListener("click",() => {
-            // Check for leading zeros
-            if (display.textContent == "0") {
-                display.textContent = "";
-            } 
-            // Check if equal sign was pressed
-            if (isEqual) {
-                display.textContent = "";
-                isEqual = false;
-            }
-            // Check if operator sign was pressed
-            if (isOperator) {
-                display.textContent = "";
-                isOperator = false;
-            }
-            // Add number to display
-            display.textContent += number.id;
+            AddNumber(number);
         })
     })
 
     // Save operator if clicked
     document.querySelectorAll(".operator").forEach(button => {
         button.addEventListener("click", () => {
-
-            // Save check against dividing by zero
-            if (operator !== "" && !isOperator) {
-                b = display.textContent;
-                if (b != 0) {
-                    display.textContent = Math.round(operate(Number(a), operator, Number(b)) * 1000) / 1000;
-                } else {
-                    display.textContent = operate(Number(a), operator, Number(b));
-                }
-            }
-            a = display.textContent;
-            operator = button.id;
-            isOperator = true;
+            AddOperator(button);
         })
     })
 
-    // Show the solution
+    // Equal sign pressed
     document.querySelector(".equal").addEventListener("click", () => {
+        equalize();
+    })
+
+    // Using dot to type decimal numbers
+    document.querySelector(".dot").addEventListener("click", () => {
+        addDot();
+    })
+
+    // Remove the last character
+    document.querySelector(".backspace").addEventListener("click", () => {
+        removeLast();
+    })
+
+
+
+
+
+    // Clears everything
+    function clear () {
+        display.textContent = "0";
+        a = 0;
+        b = 0;
+        let isOperator = false;
+        let isEqual = false;
+        let operator = "";
+    }
+
+
+    // Add operator when clicked
+    function AddOperator (button) {
+        
         if (operator !== "" && !isOperator) {
             b = display.textContent;
-            // Save check against dividing by zero
-            if (b != 0) {
-                display.textContent = Math.round(operate(Number(a), operator, Number(b)) * 1000) / 1000;
-            } else {
-                display.textContent = operate(Number(a), operator, Number(b));
-            }
+            evaluate();
+        }
+        a = display.textContent;
+        operator = button.id;
+        isOperator = true;
+    }
+
+
+    // Add number to display when pressed
+    function AddNumber (number) {
+        // Check for leading zeros
+        if (display.textContent == "0") {
+            display.textContent = "";
+        } 
+        // Check if equal sign was pressed
+        if (isEqual) {
+            display.textContent = "";
+            isEqual = false;
+        }
+        // Check if operator sign was pressed
+        if (isOperator) {
+            display.textContent = "";
+            isOperator = false;
+        }
+        // Add number to display
+        display.textContent += number.id;
+    }
+
+
+    // Show the result when equal sign pressed
+    function equalize () {
+        if (operator !== "" && !isOperator) {
+
+            b = display.textContent;
+            evaluate();
             // Reset
             isEqual = true;
             a = 0;
             b = 0;
             operator = "";
         }
-    })
+    }
 
-    // Using dot to type decimal numbers
-    document.querySelector(".dot").addEventListener("click", () => {
 
-        // Allow ty type dot only if not on screen
-        if (!display.textContent.includes('.') || isOperator) {
+    function evaluate () {
+        // Save check against dividing by zero
+        if (b != 0) {
+            display.textContent = Math.round(operate(Number(a), operator, Number(b)) * 1000) / 1000;
+        } else {
+            display.textContent = operate(Number(a), operator, Number(b));
+        }
+    }
+
+
+    // Add decimal dot when clicked
+    function addDot () {
+        // Allow to type dot only if not on screen
+        if (!display.textContent.includes('.') || isOperator || isEqual) {
             // If operator pressed, new line
-            if (isOperator) {
+            if (isOperator || isEqual) {
                 display.textContent = "0";
                 isOperator = false;
+                isEqual = false;
             }
             display.textContent += ".";
         }
-    })
+    }
+
+
+    // Removes last character
+    function removeLast () {
+
+        display.textContent = display.textContent.slice(0, -1);
+        // If not empty display
+        if (display.textContent == "") {
+            display.textContent = "0";
+        }
+    }
 })
 
 
